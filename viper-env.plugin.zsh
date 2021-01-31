f5abb8b7d8c9ceb582961d52c907b9fe78ad4142
Daniel Kaminski de Souza <51767368+DanielAtKrypton@users.noreply.github.com>
2021-01-31 07:44:31 +0000

    Update and rename viper_env.plugin.zsh to viper-env.plugin.zsh

--- viper-env.plugin.zsh
+++ viper-env.plugin.zsh
@@ -1,14 +1,14 @@
-__viper_env_help () {
+__viper-env_help () {
     printf 'Example usage:                     
-  viper_env help, h           - show help 
+  viper-env help, h           - show help 
 '
 }
 
-viper_env_runner() {
+viper-env_runner() {
   if [[ $@ == "help" || $@ == "h" ]]; then 
-    __viper_env_help
+    __viper-env_help
   #elif [[ $@ == "something" || $@ == "alias" ]]; then 
   fi
 }
 
-alias viper_env='viper_env_runner'
+alias viper_env='viper-env_runner'
